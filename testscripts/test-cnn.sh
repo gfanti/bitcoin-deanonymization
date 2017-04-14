21b54a1beffe3e3ab09baabd4916431c5f5c9f3e
nickylimjj <nickylimjj.nl@gmail.com>
2017-04-14 14:56:55 -0500

    cnn logs

--- testscripts/test-cnn.sh
+++ testscripts/test-cnn.sh
@@ -2,4 +2,4 @@
 # this test varies the CNN
 # 1st argument : number of additional 50,000 block datapoints
 
-python fully_connected_feed.py --runs $1 --hidden1 100 --max_steps 300000 --testname cnn
+python fully_connected_feed.py --batch_size 50 --runs $1 --hidden1 100 --hidden2 100 --max_steps 300000 --testname cnn
